dcf415e2d48d36e0ed440f72493c27573970e033
ichiaohsu <ichiao.hsu@gmail.com>
2017-03-30 20:40:09 +0800

    basic swiper

---
+++ js/albums.js
@@ -0,0 +1,112 @@
+{
+    "albums": [
+        {
+            "album_caption": "", 
+            "photos": [
+                {
+                    "src": "https://farm2.staticflickr.com/1682/26203954530_de38e67c46_c.jpg", 
+                    "description": "", 
+                    "title": ""
+                }, 
+                {
+                    "src": "https://farm2.staticflickr.com/1619/26450893486_f9b321cf94_c.jpg", 
+                    "description": "", 
+                    "title": ""
+                }, 
+                {
+                    "src": "https://farm8.staticflickr.com/7596/27578317944_9fab9c28ec_c.jpg", 
+                    "description": "", 
+                    "title": ""
+                }
+            ], 
+            "album_name": "foam"
+        }, 
+        {
+            "album_caption": "", 
+            "photos": [
+                {
+                    "src": "https://farm2.staticflickr.com/1719/25655965060_449eb54154_c.jpg", 
+                    "description": "", 
+                    "title": ""
+                }, 
+                {
+                    "src": "https://farm2.staticflickr.com/1447/25873234900_7c4cb16557_c.jpg", 
+                    "description": "", 
+                    "title": ""
+                }, 
+                {
+                    "src": "https://farm1.staticflickr.com/665/33170849922_98b377cc9b_c.jpg", 
+                    "description": "", 
+                    "title": ""
+                }, 
+                {
+                    "src": "https://farm4.staticflickr.com/3905/33302911736_2791a5eb8d_c.jpg", 
+                    "description": "", 
+                    "title": ""
+                }, 
+                {
+                    "src": "https://farm1.staticflickr.com/773/32516830714_5e29147626_c.jpg", 
+                    "description": "", 
+                    "title": ""
+                }, 
+                {
+                    "src": "https://farm1.staticflickr.com/679/33377900165_e06195c38c_c.jpg", 
+                    "description": "", 
+                    "title": ""
+                }, 
+                {
+                    "src": "https://farm3.staticflickr.com/2826/33015793110_1368fbff41_c.jpg", 
+                    "description": "", 
+                    "title": ""
+                }, 
+                {
+                    "src": "https://farm1.staticflickr.com/777/33243188972_e1e5896d8c_c.jpg", 
+                    "description": "", 
+                    "title": ""
+                }, 
+                {
+                    "src": "https://farm3.staticflickr.com/2853/33015783420_dbe5a2a980_c.jpg", 
+                    "description": "", 
+                    "title": ""
+                }, 
+                {
+                    "src": "https://farm3.staticflickr.com/2862/33270960241_c92604bc83_c.jpg", 
+                    "description": "", 
+                    "title": ""
+                }
+            ], 
+            "album_name": "Portrait Exhibition held in the cities"
+        }, 
+        {
+            "album_caption": "", 
+            "photos": [
+                {
+                    "src": "https://farm2.staticflickr.com/1594/24154403441_2d459b1c92_c.jpg", 
+                    "description": "", 
+                    "title": "Frozen garlics"
+                }, 
+                {
+                    "src": "https://farm2.staticflickr.com/1581/24154385941_427a371531_c.jpg", 
+                    "description": "", 
+                    "title": "White gloves"
+                }, 
+                {
+                    "src": "https://farm2.staticflickr.com/1482/23941329890_1fe7662080_c.jpg", 
+                    "description": "", 
+                    "title": "Golden knife"
+                }, 
+                {
+                    "src": "https://farm2.staticflickr.com/1511/24210802986_eb7c5894a2_c.jpg", 
+                    "description": "", 
+                    "title": "Top hat"
+                }, 
+                {
+                    "src": "https://farm2.staticflickr.com/1444/23608652504_7a4168976c_c.jpg", 
+                    "description": "", 
+                    "title": "Panadol Allergy SINUS Caplets"
+                }
+            ], 
+            "album_name": "Polinguistiks"
+        }
+    ]
+}
